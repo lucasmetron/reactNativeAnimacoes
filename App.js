c2c0ef3423f5e47a82f16c923ea8eddb4c4880a5
Lucas Rosa <lucasmetron@gmail.com>
2021-11-03 07:55:57 -0300

    dimensions

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Text,
   View,
@@ -8,21 +8,42 @@ import {
   SafeAreaView,
   Dimensions
 } from 'react-native';
-import styles, { colors, size } from './styles/mainStyles'
+import styles, { size } from './styles/mainStyles'
 import LinearGradient from 'react-native-linear-gradient';
 
+
 const App = () => {
+
+  const [screenSize, setScreenSize] = useState(50)
+  const { width, height } = Dimensions.get('window');
+
+  function getSize() {
+    if (height > width) {
+      return (width / 3) - 8
+    } else {
+      return (width / 6) - 8
+    }
+  }
+
+  useEffect(() => {
+    Dimensions.addEventListener("change", () => {
+      setScreenSize(getSize())
+    })
+  }, [])
+
+
+
 
   return (
     <SafeAreaView style={styles.container} >
 
       <View style={styles.viewContainer}>
-        <Text style={[styles.text, { width: size, height: size }]}>ABC</Text>
-        <Text style={[styles.text, { width: size, height: size }]}>DEF</Text>
-        <Text style={[styles.text, { width: size, height: size }]}>GHI</Text>
-        <Text style={[styles.text, { width: size, height: size }]}>ABC</Text>
-        <Text style={[styles.text, { width: size, height: size }]}>DEF</Text>
-        <Text style={[styles.text, { width: size, height: size }]}>GHI</Text>
+        <Text style={[styles.text, { width: screenSize, height: screenSize }]}>ABC</Text>
+        <Text style={[styles.text, { width: screenSize, height: screenSize }]}>DEF</Text>
+        <Text style={[styles.text, { width: screenSize, height: screenSize }]}>GHI</Text>
+        <Text style={[styles.text, { width: screenSize, height: screenSize }]}>ABC</Text>
+        <Text style={[styles.text, { width: screenSize, height: screenSize }]}>DEF</Text>
+        <Text style={[styles.text, { width: screenSize, height: screenSize }]}>GHI</Text>
       </View>
 
     </SafeAreaView>
