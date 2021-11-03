a98f7b3211a0d69e732938587bf1b47458b97ae3
Lucas Rosa <lucasmetron@gmail.com>
2021-11-03 08:54:37 -0300

    stop e reset

--- App.js
+++ App.js
@@ -15,20 +15,24 @@ const App = () => {
 
 
   function onPress() {
-    // setX(new Animated.Value(0))
-
-
     const myAnimationX = Animated.timing(x, {
       toValue: x._value === 0 ? 100 : 0, //dessa forma fica um toggle 
-      duration: 1000
+      duration: 2000
     })
 
     const myAnimationY = Animated.timing(y, {
       toValue: y._value === 0 ? 100 : 0, //dessa forma fica um toggle 
-      duration: 1000
+      duration: 2000
     })
     myAnimationX.start()
     myAnimationY.start()
+
+    setTimeout(() => {
+      myAnimationX.stop() // para a animação
+      myAnimationY.stop()
+      myAnimationX.reset() //reseta para o valor incial que foi colocado no state
+      myAnimationY.reset()
+    }, 1000)
   }
 
   return (
