7e99b80e7599020187fa4306277376acc9131686
Lucas Rosa <lucasmetron@gmail.com>
2021-11-05 07:43:27 -0300

    Easing

--- App.js
+++ App.js
@@ -3,31 +3,35 @@ import {
   Text,
   View,
   SafeAreaView,
-  Animated
+  Animated,
+  Easing,
 } from 'react-native';
 import styles, { size } from './styles/mainStyles'
 
 
 const App = () => {
 
-  const [position, setPosition] = useState(new Animated.ValueXY({ x: 0, y: 0 }))
-
+  const [position, setPosition] = useState(new Animated.Value(0))
 
   function onPress() {
     const myAnimation = Animated.timing(position, {
-      toValue: 100,
-      duration: 2000
+      toValue: position._value === 0 ? 100 : 0,
+      duration: 2000,
+      // easing: Easing.ease acelera no final
+      // easing: Easing.bounce da uma quicada no final
+      // easing: Easing.back(10) antes de ir pro lugar final, ele vai pra tras o quanto quisermor em px 
+      easing: Easing.elastic(5) //balança como se tivesse em um elastico
     })
-
 
     myAnimation.start()
   }
+
 
   return (
     <SafeAreaView style={styles.container} >
 
       <Animated.Text
-        style={[styles.viewContainer, { left: position.x, top: position.y }]}
+        style={[styles.viewContainer, { left: position }]}
         onPress={onPress}
       >
         TreinaWeb
