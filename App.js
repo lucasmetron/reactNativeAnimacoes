11528d9c5f8267f1d36bc76b2fbbb346c44e37f8
Lucas Rosa <lucasmetron@gmail.com>
2021-11-05 08:20:13 -0300

    cores e degs

--- App.js
+++ App.js
@@ -14,24 +14,30 @@ const App = () => {
   const [position, setPosition] = useState(new Animated.Value(0))
 
   function onPress() {
+
     const myAnimation = Animated.timing(position, {
-      toValue: 100,
-      duration: 1000,
+      toValue: 1,
+      duration: 2000,
+      easing: Easing.elastic(5)
     })
 
     myAnimation.start()
+
+
   }
 
   const myLeftAnimation = position.interpolate({
-    inputRange: [0, 20, 40, 60, 80, 100], //são as porcetagens de 0 a 100
-    outputRange: [0, 100, 50, 80, 0, 100] // valores a qual eu quero que esteja quando chegar na porcentagem do inputRange
+    inputRange: [0, 1],
+    // outputRange: ['red', 'blue']
+    outputRange: ['0deg', '360deg']
   })
 
   return (
     <SafeAreaView style={styles.container} >
 
       <Animated.Text
-        style={[styles.viewContainer, { left: myLeftAnimation }]}
+        // style={[styles.viewContainer, { backgroundColor: myLeftAnimation }]}
+        style={[styles.viewContainer, { transform: [{ rotate: myLeftAnimation }] }]}
         onPress={onPress}
       >
         TreinaWeb
