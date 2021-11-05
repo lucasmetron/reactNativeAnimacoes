544a6cb0cd587463b00206334afcafdae6c85213
Lucas Rosa <lucasmetron@gmail.com>
2021-11-05 08:00:31 -0300

    interpolate

--- App.js
+++ App.js
@@ -15,23 +15,23 @@ const App = () => {
 
   function onPress() {
     const myAnimation = Animated.timing(position, {
-      toValue: position._value === 0 ? 100 : 0,
-      duration: 2000,
-      // easing: Easing.ease acelera no final
-      // easing: Easing.bounce da uma quicada no final
-      // easing: Easing.back(10) antes de ir pro lugar final, ele vai pra tras o quanto quisermor em px 
-      easing: Easing.elastic(5) //balança como se tivesse em um elastico
+      toValue: 100,
+      duration: 1000,
     })
 
     myAnimation.start()
   }
 
+  const myLeftAnimation = position.interpolate({
+    inputRange: [0, 20, 40, 60, 80, 100], //são as porcetagens de 0 a 100
+    outputRange: [0, 100, 50, 80, 0, 100] // valores a qual eu quero que esteja quando chegar na porcentagem do inputRange
+  })
 
   return (
     <SafeAreaView style={styles.container} >
 
       <Animated.Text
-        style={[styles.viewContainer, { left: position }]}
+        style={[styles.viewContainer, { left: myLeftAnimation }]}
         onPress={onPress}
       >
         TreinaWeb
