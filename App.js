e5154e5c7cfb03cf57896f9bcc4244bdef2c4b4e
Lucas Rosa <lucasmetron@gmail.com>
2021-11-09 08:49:08 -0300

    animated.parallel

--- App.js
+++ App.js
@@ -4,39 +4,55 @@ import {
   View,
   SafeAreaView,
   Animated,
-  Easing,
+  Switch,
+
 } from 'react-native';
 import styles, { size } from './styles/mainStyles'
 
-
 const App = () => {
 
-  const [position, setPosition] = useState(new Animated.Value(0))
+  const position = new Animated.Value(0)
+  const position2 = new Animated.Value(0)
+
 
   function onPress() {
-    const myAnimation = Animated.spring(position, {
+    position.setValue(0)
+    position2.setValue(0)
+
+    const myAnimation = Animated.timing(position, {
       toValue: 100,
-      friction: 1,
-      // bouncines: 20, quicada no final
-      // speed: 20 velocidade do movimento
-      tension: 100 // força que a mola é acionada
+      duration: 2000,
     })
 
-    myAnimation.start()
+    const myAnimation2 = Animated.timing(position2, {
+      toValue: -100,
+      duration: 2000,
+    })
 
+    Animated.parallel([ //dessa forma as duas animações iram acontecer no memso tempo
+      myAnimation,
+      myAnimation2
+    ]).start()
+
+    // myAnimation.start() antes usavamos assim, mas isso não garante a execução das duas ao mesmo tempo
+    // myAnimation2.start()
   }
-
-
 
   return (
     <SafeAreaView style={styles.container} >
-
       <Animated.Text
         style={[styles.viewContainer, { left: position }]}
+      >
+        TreinaWeb
+      </Animated.Text>
+
+      <Animated.Text
+        style={[styles.viewContainer, { left: position2 }]}
         onPress={onPress}
       >
         TreinaWeb
       </Animated.Text>
+
 
     </SafeAreaView>
   );
