e9d473617dc01d585c9e4bf48f5c74b9c6cb9e87
Lucas Rosa <lucasmetron@gmail.com>
2021-11-11 17:13:49 -0300

    addListener

--- App.js
+++ App.js
@@ -8,53 +8,18 @@ import {
 
 } from 'react-native';
 import styles, { size } from './styles/mainStyles'
+import MyButton from './components/MyButton';
 
 const App = () => {
 
-  const [canPress, setCanPress] = useState(true)
-  const position = new Animated.Value(0)
-  const position2 = new Animated.Value(0)
-
-  async function onPress() {
-
-    if (canPress) {
-      setCanPress(false) //para não permitir entrar na animação novamente caso pressionado o botão
-      position.setValue(-100)
-      position2.setValue(0)
-
-      const myAnimation = Animated.timing(position, {
-        toValue: 0,
-        duration: 1000,
-      })
-
-      const myAnimation2 = Animated.spring(position2, {
-        toValue: 100,
-        friction: 1,
-      })
-
-      await Animated.stagger(1000, [
-        myAnimation,
-        myAnimation2,
-      ]).start(() => { setCanPress(true) }) // no fim da animação volta o valor para true
-    }
+  function onFullPress() {
+    console.log('Clique Feito')
   }
+
 
   return (
     <SafeAreaView style={styles.container} >
-      <Animated.Text
-        style={[styles.viewContainer, { top: position }]}
-      >
-        TreinaWeb
-      </Animated.Text>
-
-      <Animated.Text
-        style={[styles.viewContainer, { top: position2 }]}
-        onPress={onPress}
-      >
-        TreinaWeb
-      </Animated.Text>
-
-
+      <MyButton onFullPress={onFullPress}>Lucas Rosa </MyButton>
     </SafeAreaView>
   );
 };
